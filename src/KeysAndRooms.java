e18a8cf1f41d9d6bfa01d0484c82da4ffdb2da13
Test <justnisar@gmail.com>
2024-04-26 13:54:47 -0700

    Added first graph

---
+++ src/KeysAndRooms.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class KeysAndRooms {
+
+    private boolean canVisitAll(boolean[] visited){
+        for(boolean node : visited){
+            if(node == false){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        // 0th room can always be visited
+        dfs(0, rooms, visited);
+        return canVisitAll(visited);
+    }
+
+    private void dfs(int currentNode, List<List<Integer>> rooms, boolean[] visited) {
+        if(visited[currentNode] == false) {
+            visited[currentNode] = true;
+            for (int key : rooms.get(currentNode)) {
+                visited[key] = true;
+                dfs(key, rooms, visited);
+            }
+        }
+    }
+
+}
