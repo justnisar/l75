788f2ed0ed99e4ca026eaa61ad78dabb01ec07a6
Test <justnisar@gmail.com>
2024-05-26 16:46:37 -0700

    More code

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -3,40 +3,59 @@ import java.util.List;
 
 public class StringCompression {
 
-    private List<Character> getConvertedCharacterList(int number){
+    List<Character> getDigitsFromNumber(int number){
         List<Character> result = new ArrayList<>();
         if(number == 1){
             return result;
         }
         while(number != 0){
-            int remainder = number % 10;
-            char ch = (char) (remainder + 48);
-            result.set(0, ch);
-            number /= 10;
+            int digit = number % 10;
+            number = number / 10;
+            char digitCh = (char) (digit + '0');
+            result.add(0, digitCh);
         }
         return result;
     }
 
-    public int compress(char[] chars) {
+    private int rewriteArray(int pointer, char[] chars, char prev, int count){
+        chars[pointer] = prev;
+        pointer++;
+        List<Character> numberChars = getDigitsFromNumber(count);
+        for(char ch : numberChars){
+            chars[pointer] = ch;
+            pointer++;
+        }
+        return pointer;
+    }
 
+    public int compress(char[] chars) {
+        if(chars.length == 1){
+            return 1;
+        }
         int pointer = 0;
         int count = 1;
         char prev = chars[0];
-
+        char current = ' ';
         for(int i = 1 ; i < chars.length ; i++){
+            current = chars[i];
 
-            char current = chars[i];
             if(current == prev){
                 count++;
             }
+            // point of change
             else{
-
-
-
+                pointer = rewriteArray(pointer, chars, prev, count);
+                prev = current;
+                count = 1;
             }
-
         }
+        pointer = rewriteArray(pointer, chars, current, count);
+        return pointer;
+    }
 
-        return 0;
+    public static void main(String[] args) {
+        char[] chArr = {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+        StringCompression obj = new StringCompression();
+        System.out.println(obj.compress(chArr));
     }
 }
